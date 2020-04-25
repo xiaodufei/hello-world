ce69ab595c09b604f2d338dddd3cf32b8336942b
xiaodufei <dufei0206@gmail.com>
2020-04-26 01:11:02 +0200

    This is version 2 I added the comment

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# hello-world
+### hello-world
 
 This is my first project in Github！
--- hello,world.c
+++ hello,world.c
@@ -3,8 +3,11 @@
 int main ()
 {
 
+    // this is the Github demo project
 	printf("Hello,world!\n");
 	printf("Hello,world!\n");
+
+
 
 	return 0 ;
 }
